8b4e4d5458b7bdda067b4ac8c9e4f079d8778de6
Megan Jezewski <meganjezewski@gmail.com>
2018-06-26 20:17:16 -0400

    final updates/ polishing

--- editFile.py
+++ editFile.py
@@ -4,12 +4,12 @@ import json
 import sys
 from itertools import *
 
-for i in range(1,4): 
+for i in range(1,5): 
     servConfFile = sys.argv[i]
     f = open(servConfFile)
     filedata = f.read()
     f.close()
-    fixeddata = filedata.replace("german",sys.argv[4])
+    fixeddata = filedata.replace("exmpl",sys.argv[5])
     f = open(servConfFile, "w")
     f.write(fixeddata)
     f.close()
--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ else
 	echo "$1 will be the name of the directory you have created on the server."
 fi
 mkdir ~/public_html/$1
-cp -rp ~/public_html/german/* ./$1
+# initial copy of materials
+cp -rp ~/public_html/exmpl/* ./$1
 chmod 755 $1
+# initial copy of cgi-bin materials
 mkdir ~/public_html/cgi-bin/$1
 chmod 755 ~/public_html/cgi-bin/$1
-cp -p ~/public_html/cgi-bin/german/* ~/public_html/cgi-bin/$1/
+cp -p ~/public_html/cgi-bin/exmpl/* ~/public_html/cgi-bin/$1/
 chmod 755 ~/public_html/cgi-bin/$1/
-cp $2 ~/public_html/$1/data_includes/
-rm ~/public_html/$1/data_includes/germag.js
+# add personalized data file
+cp $2 ~/public_html/$1/data_includes
 chmod 755 ~/public_html/$1/data_includes/*
-#rm -f ~/public_html/$1/results/*
 rm -f ~/public_html/$1/cache/*
 echo "completed"
 
-#python editFile.py ~/public_html/$1/server_conf.py $user $1
-python editFile.py ~/public_html/$1/other_includes/main.js ~/public_html/$1/www/experiment.html ~/public_html/cgi-bin/$1/server_conf.py $1
+python editFile.py ~/public_html/$1/other_includes/main.js ~/public_html/$1/www/experiment.html ~/public_html/$1/www/server.cgi ~/public_html/cgi-bin/$1/server_conf.py $1
